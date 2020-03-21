fcef9a2cc4dfcb57cc01d12110651fd65c192c95
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2020-03-21 16:57:34 +0600

    fixed pg function

--- src/libs/pg.ts
+++ src/libs/pg.ts
@@ -1,23 +1,21 @@
 const { Pool } = require('pg')
-const pool = new Pool()
+const pool = new Pool({ max: 100 })
+
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err)
+    // process.exit(-1)
+})
 
 export async function pgQuery(sql) {
-    // the pool will emit an error on behalf of any idle clients
-    // it contains if a backend error or network partition happens
-    pool.on('error', (err, client) => {
-        console.error('Unexpected error on idle client', err)
-        process.exit(-1)
-    })
-    // async/await - check out a client
-    ;(async () => {
-        const client = await pool.connect()
-        try {
-            const res = await client.query(sql, [])
-            console.log(res.rows[0])
-        } finally {
-            // Make sure to release the client before any error handling,
-            // just in case the error handling itself throws an error.
-            client.release()
-        }
-    })().catch(err => console.log(err.stack))
+    const start = Date.now()
+    const client = await pool.connect()
+    try {
+        const res = await client.query(sql, [])
+        // console.log(res.rows[0])
+    } finally {
+        // Make sure to release the client before any error handling,
+        // just in case the error handling itself throws an error.
+        client.release()
+    }
+    console.log('spent ', Date.now() - start, 'ms')
 }
