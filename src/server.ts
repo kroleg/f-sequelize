87f3ea4df346d76c6329dfdfef9b767e8f159850
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2020-03-21 17:01:11 +0600

    removed koa; added detection of event loop blocking; switched from ts-node-dev to ts-node

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,30 @@
-import Koa from 'koa';
+import http from 'http';
+import blocked from 'blocked-at';
 
 import { sequelize } from './libs/seq'
 import { pgQuery } from './libs/pg'
 
-const app = new Koa();
+blocked((time, stack) => {
+    console.log(`Blocked for ${time}ms, operation started here:`, stack)
+}, { threshold: 100 })
 
-app.use(async (ctx) => {
-    const sql = `SELECT * FROM clubs WHERE id > 5000`
+const sql = `SELECT * FROM clubs WHERE id > 5000`
+
+const port = 3000
+const requestHandler = (request, response) => {
     if (process.env.PG) {
-        await pgQuery(sql)
+        pgQuery(sql).then(r => response.end('OK'))
     } else {
-        await sequelize.query(sql)
+        sequelize.query(sql).then(r => response.end('OK'))
     }
-    ctx.body = 'ok'
+}
+
+const server = http.createServer(requestHandler)
+
+// @ts-ignore
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
 })
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.info(`App is up on http://localhost:${port}`);
-    console.info(`Using ${process.env.PG ? `PG` : 'SEQUELIZE'}`);
-});
